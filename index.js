aab533cb3a5b0916c6173801277935acc31585b4
Allister Driscoll <allisterdrisc@gmail.com>
2025-05-08 10:45:35 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+  console.log('Webhook received!');
+  console.log(req.headers);
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
